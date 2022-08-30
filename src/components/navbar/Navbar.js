35f6c911a8c8e850521de0e2b2e921db25d7db3d
Godspower Idiakose <idiakeg@gmail.com>
2022-08-30 02:42:24 +0100

    created and styled the navbar

---
+++ src/components/navbar/Navbar.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import { GoThreeBars } from "react-icons/go";
+import { MdOutlineClose } from "react-icons/md";
+import logo from "../../images/logo.png";
+import { links } from "../../data";
+import "./Navbar.css";
+
+const Navbar = () => {
+	const [isNavShowing, setIsNavShowing] = useState(false);
+	return (
+		<nav>
+			<div className="container nav__container">
+				<Link to="/">
+					<img src={logo} alt="Nav Link" className="logo" />
+				</Link>
+				<ul
+					className={`nav__links ${isNavShowing ? "show__nav" : "hide__nav"}`}
+				>
+					{links.map(({ name, path }, index) => {
+						return (
+							<li key={index}>
+								<NavLink
+									to={path}
+									className={({ isActive }) =>
+										isActive ? "active-nav" : undefined
+									}
+								>
+									{name}
+								</NavLink>
+							</li>
+						);
+					})}
+				</ul>
+				<button
+					className="nav__toggle-btn"
+					onClick={() => setIsNavShowing((prev) => !prev)}
+				>
+					{isNavShowing ? <MdOutlineClose /> : <GoThreeBars />}
+				</button>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
