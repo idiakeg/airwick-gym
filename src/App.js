197fedf411266a7afe1c2e7de0d215a9d802336f
Godspower Idiakose <idiakeg@gmail.com>
2022-08-30 03:12:24 +0100

    implemented the routing for the website

--- src/App.js
+++ src/App.js
@@ -12,13 +12,22 @@ function App() {
 	return (
 		<BrowserRouter>
 			<Navbar />
-			<Home />
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/contact" element={<Contact />} />
+				<Route path="/gallery" element={<Gallery />} />
+				<Route path="/plans" element={<Plans />} />
+				<Route path="/trainers" element={<Trainers />} />
+				<Route path="*" element={<NotFound />} />
+			</Routes>
+			{/* <Home />
 			<About />
 			<Contact />
 			<Gallery />
 			<Plans />
 			<Trainers />
-			<NotFound />
+			<NotFound /> */}
 		</BrowserRouter>
 	);
 }
