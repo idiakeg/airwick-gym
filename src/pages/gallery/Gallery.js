f326c8a82c621bcbe01f7bf36bf096acfdc3e257
Godspower Idiakose <idiakeg@gmail.com>
2022-09-02 05:45:04 +0100

    created the gallery page

--- src/pages/gallery/Gallery.js
+++ src/pages/gallery/Gallery.js
@@ -1,7 +1,33 @@
+import Header from "../../components/Header";
+import HeaderImage from "../../images/header_bg_3.jpg";
 import "./Gallery.css";
 
 const Gallery = () => {
-	return <div>Gallery</div>;
+	// const galleryLength = 15
+	let images = [];
+	for (let i = 1; i <= 15; i++) {
+		images.push(require(`../../images/gallery${i}.jpg`));
+	}
+
+	console.log(images);
+
+	return (
+		<>
+			<Header image={HeaderImage} title="Gallery">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Officiis optio
+				maxime temporibus sunt laborum deleniti.
+			</Header>
+			<section className="gallery">
+				<div className="container gallery__container">
+					{images.map((image, index) => (
+						<article key={index}>
+							<img src={image} alt={`Gallery ${index + 1}`} />
+						</article>
+					))}
+				</div>
+			</section>
+		</>
+	);
 };
 
 export default Gallery;
