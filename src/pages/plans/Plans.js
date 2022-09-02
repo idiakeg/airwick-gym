6a408ce8be3fd2bed8b834b2f52c99181f5c305c
Godspower Idiakose <idiakeg@gmail.com>
2022-09-02 15:35:15 +0100

    created and styled the palns page

--- src/pages/plans/Plans.js
+++ src/pages/plans/Plans.js
@@ -1,7 +1,37 @@
+import Header from "../../components/Header";
+import HeaderImage from "../../images/header_bg_4.jpg";
+import Card from "../../UI/Card";
+import { plans } from "../../data";
 import "./Plans.css";
 
 const Plans = () => {
-	return <div>Plans</div>;
+	return (
+		<>
+			<Header image={HeaderImage} title="Membership Plans">
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat nam
+				esse architecto impedit, delectus asperiores.
+			</Header>
+			<section className="plans">
+				<div className="container plans__container">
+					{plans.map(({ id, name, desc, price, features }) => (
+						<Card className="plan" key={id}>
+							<h3>{name}</h3>
+							<small>{desc}</small>
+							<h1>{`$${price}`}</h1>
+							<h2>/mo</h2>
+							<h4>Features</h4>
+							{features.map(({ feature, available }, index) => (
+								<p key={index} className={available ? "" : "disabled"}>
+									{feature}
+								</p>
+							))}
+							<button className="btn lg">Choose Plan</button>
+						</Card>
+					))}
+				</div>
+			</section>
+		</>
+	);
 };
 
 export default Plans;
