f6400de264fb4a3fa1af0f34b69ea8e0f8eace41
Godspower Idiakose <idiakeg@gmail.com>
2022-09-03 19:43:47 +0100

    made changes to the route

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 		<BrowserRouter>
 			<Navbar />
 			<Routes>
-				<Route path="/" element={<Home />} />
+				<Route path="" element={<Home />} />
 				<Route path="/about" element={<About />} />
 				<Route path="/contact" element={<Contact />} />
 				<Route path="/gallery" element={<Gallery />} />
